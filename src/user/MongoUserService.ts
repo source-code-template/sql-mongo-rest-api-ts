2ef6eb4ddb70babe020d8ac82df36794c626979c
Duc Nguyen <minhduc140583@gmail.com>
2021-11-21 22:47:58 +0700

    Format code

--- src/user/MongoUserService.ts
+++ src/user/MongoUserService.ts
@@ -9,9 +9,4 @@ export class MongoUserService extends Service<User, string, UserFilter> {
   constructor(find: (s: UserFilter, limit?: number, offset?: number | string, fields?: string[]) => Promise<SearchResult<User>>, db: Db) {
     super(find, db, 'users', userModel.attributes);
   }
-  /*
-  all(): Promise<Location[]> {
-    return findWithMap<Location>(this.collection, {});
-  }
-  */
 }
